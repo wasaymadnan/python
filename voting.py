0d31f2d41dc1f942bfb749c61d82acfbd6cce56d
wasaymadnan <wasaymadnan@gmail.com>
2025-08-11 11:28:35 +0100

    Update .DS_Store file.

---
+++ voting.py
@@ -0,0 +1,17 @@
+jvote = 0
+avote = 0
+svote = 0
+
+vote = input ("Please enter voter's name : ")
+if vote == "john":
+    jvote = jvote + 1
+elif vote == "adam":
+    avote = avote + 1
+elif vote == "sam":
+    svote = svote + 1
+else:
+    print ("Please enter a valid entry!")
+
+print ("john total vote :",jvote)
+print ("adam total vote :",avote)
+print ("sam total vote :",svote)
